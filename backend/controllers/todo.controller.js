6d7d055a9ccb2b606c29ce99a983256fbc781598
Aman Kumar <amankumaroo784@gmail.com>
2024-09-30 10:02:44 +0530

    todo controller fixes

--- backend/controllers/todo.controller.js
+++ backend/controllers/todo.controller.js
@@ -1,42 +1,84 @@
-import Todo from '../models/todo.model.js';
+import Todo from "../models/todo.model.js";
 
 export const getAllTodo = async (req, res) => {
-  const todoList = await Todo.find({});
-  if(!todoList) {
-    console.log(`error in fetching data from database: ${err}`);
-    return;
+  try {
+    const todoList = await Todo.find({});
+    return res.status(200).json({
+      success: true,
+      data: todoList,
+    });
+  } catch (error) {
+    console.log(`error in fetching data from database: ${error}`);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
-  return res.render("todo", {
-    title: "Todo App",
-    todo_list: todoList,
-  });
 };
 
-export const createTodo = (req, res) => {
-  Todo.create(
-    {
+export const createTodo = async (req, res) => {
+  try {
+    const newTodoTask = await Todo.create({
       descriptions: req.body.todoText,
       date: req.body.date,
       category: req.body.category,
-    },
-    (err, newTodoTask) => {
-      if (err) {
-        console.log(`error in creating new task ${err}`);
-        return;
-      }
-      return res.redirect("back");
-    }
-  );
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Todo created successfully",
+      data: newTodoTask,
+    });
+  } catch (error) {
+    console.log(`Error in creating new task: ${error}`);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to create todo",
+    });
+  }
 };
 
-export const deleteTodo = (req, res) => {
-  let id = req.query.id;
-  Todo.findByIdAndDelete(id, function (err) {
-    if (err) {
-      console.log(`error in deleting from the database ${err}`);
-      return;
-    }
-
-    return res.redirect("back");
-  });
+export const deleteTodo = async (req, res) => {
+  try {
+    const id = req.query.id;
+    await Todo.findByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      message: "Todo deleted successfully",
+    });
+  } catch (error) {
+    console.log(`Error in deleting from the database: ${error}`);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to delete todo",
+    });
+  }
 };
+
+// export const createTodo = (req, res) => {
+//   Todo.create(
+//     {
+//       descriptions: req.body.todoText,
+//       date: req.body.date,
+//       category: req.body.category,
+//     },
+//     (err, newTodoTask) => {
+//       if (err) {
+//         console.log(`error in creating new task ${err}`);
+//         return;
+//       }
+//       return res.redirect("back");
+//     }
+//   );
+// };
+
+// export const deleteTodo = (req, res) => {
+//   let id = req.query.id;
+//   Todo.findByIdAndDelete(id, function (err) {
+//     if (err) {
+//       console.log(`error in deleting from the database ${err}`);
+//       return;
+//     }
+
+//     return res.redirect("back");
+//   });
+// };
