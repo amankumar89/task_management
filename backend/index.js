5f0058d78f7632d0f9ccef943ad4d4d619551512
Aman Kumar <amankumaroo784@gmail.com>
2024-10-02 12:50:06 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,29 @@ const PORT = process.env.PORT || 50001;
 import { connectDB } from "./config/mongoose.js";
 
 import todoRoutes from "./routes/todo.route.js";
+
+app.set("trust proxy", true);
+
+// Enable CORS
+app.use(cors());
+
+// Configuration object for different environments
+const config = {
+  development: {
+    BASE_URL: `http://localhost:${PORT}`,
+  },
+  production: {
+    BASE_URL:
+      process.env.BASE_URL ||
+      "https://todo-app-git-main-amankumar89s-projects.vercel.app",
+  },
+};
+
+// Determine current environment
+const currentConfig =
+  process.env.NODE_ENV === "production"
+    ? config.production
+    : config.development;
 
 // urlencoded for the post method
 app.use(express.json());
